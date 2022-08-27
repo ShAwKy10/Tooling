306399152763c02a41027c67910a9647669f6f56
Shawky <amamasa121212@gmail.com>
2022-08-27 08:21:51 +0200

    adding hidder file

---
+++ dt.h
@@ -0,0 +1,15 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+typedef unsigned char u8_t;
+typedef unsigned short int u16_t;
+typedef unsigned long int u32_t;
+
+typedef signed char s8_t;
+typedef signed char s16_t;
+typedef signed char s32_t;
+
+typedef float f32_t;
+typedef double f64_t;
+
+#endif
